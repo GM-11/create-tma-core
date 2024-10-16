83ade73fe013c62130acba954397b18ca0af2281
GM-11 <mathuyrgopal@gmail.com>
2024-10-16 20:22:29 +0530

    updated bot folder

--- template/bot/index.ts
+++ template/bot/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { Telegraf } from "telegraf";
 
-const BOT_TOKEN = process.env.BOT_TOKEN || "";
+const BOT_TOKEN = process.env.BOT_TOKEN;
 
 if (!BOT_TOKEN) {
   throw new Error("BOT_TOKEN is not set");
