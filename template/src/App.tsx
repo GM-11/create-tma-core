ed7bc831138930f6aad2511c930ac71c879c3acd
GM-11 <mathuyrgopal@gmail.com>
2024-10-22 12:30:34 +0530

    reverted back to wallet connect

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,35 +1,8 @@
-import { useSDK } from "@metamask/sdk-react";
-import React, { useState } from "react";
-
-const App: React.FC = () => {
-  const [account, setAccount] = useState<string>();
-  const { sdk, connected, chainId } = useSDK();
-
-  const connect = async () => {
-    try {
-      const accounts = await sdk?.connect();
-      setAccount(accounts?.[0]);
-    } catch (err) {
-      console.warn("failed to connect..", err);
-    }
-  };
-
+export default function App() {
   return (
-    <div className="App">
-      <button style={{ padding: 10, margin: 10 }} onClick={connect}>
-        Connect
-      </button>
-      {connected && (
-        <div>
-          <>
-            {chainId && `Connected chain: ${chainId}`}
-            <p></p>
-            {account && `Connected account: ${account}`}
-          </>
-        </div>
-      )}
+    <div>
+      <h1>Telegram mini app on Core </h1>
+      <w3m-button />
     </div>
   );
-};
-
-export default App;
+}
--- template/src/main.tsx
+++ template/src/main.tsx
@@ -2,41 +2,28 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-// import { createAppKit } from "@reown/appkit/react";
-// import { EthersAdapter } from "@reown/appkit-adapter-ethers";
-// import { coreDao } from "@reown/appkit/networks";
-import { MetaMaskProvider } from "@metamask/sdk-react";
+import { createAppKit } from "@reown/appkit/react";
+import { EthersAdapter } from "@reown/appkit-adapter-ethers";
+import { coreDao } from "@reown/appkit/networks";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MetaMaskProvider
-      debug={false}
-      sdkOptions={{
-        dappMetadata: {
-          name: "Example React Dapp",
-          url: window.location.href,
-        },
-        infuraAPIKey: "a0c7145a37044376a3cec71eac4069e7",
-        // Other options.
-      }}
-    >
-      <App />
-    </MetaMaskProvider>
+    <Root />
   </StrictMode>,
 );
 
-// function Root() {
-//   // createAppKit({
-//   //   adapters: [new EthersAdapter()],
-//   //   networks: [coreDao],
-//   //   metadata: {
-//   //     name: "Ton + Core starter",
-//   //     description: "AppKit Example",
-//   //     url: "https://reown.com/appkit", // origin must match your domain & subdomain
-//   //     icons: ["https://assets.reown.com/reown-profile-pic.png"],
-//   //   },
-//   //   projectId: "7594a316c1c467100dafe6fe766c3914",
-//   // });
+function Root() {
+  createAppKit({
+    adapters: [new EthersAdapter()],
+    networks: [coreDao],
+    metadata: {
+      name: "Ton + Core starter",
+      description: "AppKit Example",
+      url: "https://reown.com/appkit", // origin must match your domain & subdomain
+      icons: ["https://assets.reown.com/reown-profile-pic.png"],
+    },
+    projectId: "7594a316c1c467100dafe6fe766c3914",
+  });
 
-//   return <App />;
-// }
+  return <App />;
+}
