ce534d212cb3b048bea115a6365582d9c9828a2a
GM-11 <mathuyrgopal@gmail.com>
2024-10-27 01:39:53 +0530

    updated deploy-app command

--- package/utils/setupSolidity.ts
+++ package/utils/setupSolidity.ts
@@ -62,7 +62,7 @@ export default async function setupSolidity(targetDir: string) {
       "compile-contracts": "npx hardhat compile",
       "deploy-contracts":
         "npx hardhat ignition deploy ./scripts/deploy.ts --network core_testnet",
-      "deploy-app": "vercel",
+      "deploy-app": "vercel --prod",
       "bot-server": "cd bot && npm run start",
       "bot-ngrok": "cd bot && ngrok http 3000",
     };
